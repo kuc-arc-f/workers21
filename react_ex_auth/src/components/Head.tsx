0cf6ab43988b9f2dfd0edecc970ef0ea878b76b3
中島浩二 <knaka0209@gmail.com>
2024-12-26 16:42:33 +0900

    up, 1226a9

--- react_ex_auth/src/components/Head.tsx
+++ react_ex_auth/src/components/Head.tsx
@@ -6,8 +6,6 @@ function Page() {
     <div>
         <a href="/" className="font-bold ms-4" > Home </a>
         <a href="/about" className="ms-2 text-gray-400"> [ about ]</a>
-        <a href="/todo11" className="font-bold ms-4" > todo11 </a>
-        <a href="/todo24" className="font-bold ms-4" > todo24 </a>
     </div>
     );
 }
